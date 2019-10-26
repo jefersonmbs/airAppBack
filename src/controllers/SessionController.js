3e99bb35edcd65cf1f7e7c175d8bbc725407e14f
Jeferson Martins <jefersontoga94@gmail.com>
2019-10-26 12:02:51 -0300

    Regra da Senha

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -4,10 +4,29 @@ const User = require('../models/User')
 module.exports= {
     async store(req,res){
         const {email}  = req.body;
+        const {senha} =  req.body;
+        
         let user = await User.findOne({email});
-        if (!user ){
+        
+        //senha Forte Letras Maiusca, Minuscula, Numeros, e caracter especial
+        //No minimo 8 Digitos
+        let senhaForte = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+        
+        //Letras M m e Numeros no minimo 6 digitos
+        let senhaMedium = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+        if(user)
+        {
+            return res.status(400).json({status: "Usuario ja cadastrado"});
+        }
+        else if(!senhaForte.test(senha))
+        {
+
+            return res.status(400).json({erro:"senha deve conter Letra Maiuscula numero e caracter especial"})
+        }
+        else if (!user ){
             user = await User.create({
-                email
+                email,
+                senha
             });
         }
         
--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require ('mongoose');
 
 const userSchema = new mongoose.Schema({
     email: String,
+    senha: String,
 });
 
 module.exports = mongoose.model('User', userSchema);
